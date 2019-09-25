d346d275948a7b0ff3aac483c0f858b941a1836e
NH <nadim@ob-soft.com>
2019-09-25 09:04:39 +0000

    Restructured the project

--- Controller/ContactController.php
+++ Controller/ContactController.php
@@ -1,6 +1,6 @@
 <?php 
 
-require_once('ContactModel.php');
+require_once('../Model/ContactModel.php');
 
 class ContactController {
     public function run() {
@@ -14,16 +14,16 @@ class ContactController {
             if($isValid) {
                 $contactModel->save();
                 $template = "contact_view_success.php";
-                include('contact_view_base.php');
+                include('../assets/views/base.php');
             }
             // IF NOT, render form with errors
             else {
                 $template = "contact_view_form.php";
-                include('contact_view_base.php');
+                include('../assets/views/base.php');
             }
         } else {
             $template = "contact_view_form.php";
-            include('contact_view_base.php');
+            include('../assets/views/base.php');
         }
     }
 }
